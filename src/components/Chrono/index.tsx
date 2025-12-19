2603bd6497cf8042790e4dda458a6599d5032ecb
Rael CALITRO <rael06@hotmail.fr>
2025-12-19 12:52:15 +0100

    Implement start and stop functionality in Chrono component; add buttons to control timer state

--- src/components/Chrono/index.tsx
+++ src/components/Chrono/index.tsx
@@ -2,23 +2,36 @@ import { useEffect, useState } from "react";
 
 export const Chrono: React.FC = () => {
   const [time, setTime] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prev) => {
-        const newTime = prev + 1;
-        console.log("time", newTime);
-        return newTime;
-      });
-    }, 1000);
+    if (isStarted) {
+      const interval = setInterval(() => {
+        setTime((prev) => {
+          const newTime = prev + 1;
+          console.log("time", newTime);
+          return newTime;
+        });
+      }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+      return () => clearInterval(interval);
+    }
+  }, [isStarted]);
+
+  const handleStart = () => {
+    setIsStarted(true);
+  };
+
+  const handleStop = () => {
+    setIsStarted(false);
+  };
 
   return (
     <div>
       <p>Chrono :</p>
       <p>{time} s.</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 };
